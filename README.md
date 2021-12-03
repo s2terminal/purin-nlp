5953b2c105b4804f186494be704e11d305cf7511
s2terminal <suzuki.sh@s2terminal.com>
2021-12-03 19:23:58 +0900

    add flake8 and autopep8

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pudding-Jigglypuff Disambiguation
+
+```
+$ docker-compose up
+```
+
+## Testing
+```
+$ docker-compose run --rm app poetry run pytest
+```
--- src/utils/base64.py
+++ src/utils/base64.py
@@ -1,7 +1,9 @@
 from base64 import b64encode, b64decode
 
+
 def base64encode(text: str) -> str:
     return b64encode(text.encode()).decode()
+
 
 def base64decode(text: str) -> str:
     return b64decode(text.encode()).decode()
